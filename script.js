d3d6a40c6ee461a8ba3054a9948f168aa966e891
Caleb Chaney <calebc347@gmail.com>
2021-02-12 15:12:35 -0600

    Added youtube API call in script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+// API Calls
+function searchMovies(movie) {
+    $("#results").empty();
+
+    // Youtube API Call
+    let youtubeApiKey = "AIzaSyDBrUxXa2vD-HgvutwEKgqgiowdM_Ex0zc";
+    let youtubeQueryURL = "https://www.googleapis.com/youtube/v3/search" + "?part=snippet&q=" + movie + "&type=video&videoCaption=closedCaption&key=" + youtubeApiKey;
+
+    $.ajax({
+        url: youtubeQueryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log(response);
+        let videoid = response.items[0].id.videoId
+        console.log(videoid);
+        let iframe = `<iframe width="560" height="315" src="https://www.youtube.com/embed/${videoid}" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
+
+        // Empty the contents, append the new video
+        $("#results").append(iframe);
+    });
+};
+
+$("#searchButton").on("click", function () {
+    event.preventDefault();
+    let inputMovie = $("#findtext").val().trim();
+
+    searchArtists(inputMovie);
+});
